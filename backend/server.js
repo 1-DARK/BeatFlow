c1c49789282227a38a00a40ddf009b906b9495ae
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-08-23 15:53:43 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -7,5 +8,7 @@ const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
   res.send("Hello world hi");
 });
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => console.log(`Server is running on port  ${PORT}`));
