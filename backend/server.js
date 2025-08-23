6d5dbbb5cb8919cbce99360e9ae45833598dbaeb
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-08-23 15:39:33 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
-const express = require("express");
-
+import express from "express";
+import dotenv from "dotenv";
 const app = express();
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello world hi");
 });
 
-app.listen(3000, () => console.log("Server is running on port 3000"));
+app.listen(PORT, () => console.log(`Server is running on port  ${PORT}`));
