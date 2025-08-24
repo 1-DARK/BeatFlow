53680290a8a503e657af183a14963b5c2c725796
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-08-24 23:01:56 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import { connectToMongoDB } from "./db/connectToMongoDB.js";
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -11,4 +12,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/auth", authRoutes);
 
-app.listen(PORT, () => console.log(`Server is running on port  ${PORT}`));
+app.listen(PORT, () => {
+  console.log("Server started at http://localhost:" + PORT);
+  connectToMongoDB();
+});
