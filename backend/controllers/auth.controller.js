9531a0160d070cb1226c74b8a618d8faa6f753bc
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-08-24 22:32:42 +0530

    initial commit

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,11 +1,11 @@
+export const signup = (req, res) => {
+  console.log("signupUser");
+};
+
 export const login = (req, res) => {
   console.log("loginUser");
 };
 
 export const logout = (req, res) => {
   console.log("logoutUser");
-};
-
-export const signup = (req, res) => {
-  console.log("signupUser");
 };
