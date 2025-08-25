981ebc87f60cd756e541ceeb4d0e9bac933ff5ac
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-08-25 22:49:29 +0530

    initial commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-dotenv.config();
-
 app.use(express.json());
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log("Server started at http://localhost:" + PORT);
