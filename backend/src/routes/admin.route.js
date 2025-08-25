64f9e989a8adf82871f169e186108f927e28fa16
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-08-26 00:53:39 +0530

    initial commit

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -1,8 +1,7 @@
 import { Router } from "express";
+import { getAdmin } from "../controller/admin.controller.js";
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("admin");
-});
+router.get("/", getAdmin);
 
 export default router;
