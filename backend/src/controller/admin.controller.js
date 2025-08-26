d06a5f3c87ee08c36c11cd880eb05c51d486fb2b
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-08-26 22:20:08 +0530

    initial commit

--- backend/src/controller/admin.controller.js
+++ backend/src/controller/admin.controller.js
@@ -1,6 +1,18 @@
 import { Album } from "../models/album.model.js";
 import { Song } from "../models/song.model.js";
-
+import cloudinary from "../lib/cloudinary.js";
+// helper functions for cloudinary uploads
+const uploadToCloudinary = async (file) => {
+  try {
+    const result = await cloudinary.uploader.upload(file.tempFilePath, {
+      resource_type: "auto",
+    });
+    return result.secure_url;
+  } catch (error) {
+    console.log("Error in uploadToCloudinary", error);
+    throw new Error("Error uploading to cloudinary");
+  }
+};
 export const createSong = async (req, res, next) => {
   try {
     if (!req.files || !req.files.audioFile || !req.files.imageFile) {
@@ -9,6 +21,9 @@ export const createSong = async (req, res, next) => {
     const { title, artist, albumId, duration } = req.body;
     const audioFile = req.files.audioFile;
     const imageFile = req.files.imageFile;
+
+    const audioUrl = await uploadToCloudinary(audioFile); // upload audio
+    const imageUrl = await uploadToCloudinary(imageFile); // upload image
     const song = new Song({
       title,
       artist,
@@ -32,4 +47,9 @@ export const createSong = async (req, res, next) => {
     console.log("Error in createSong", error);
     next(error);
   }
+};
+export const deleteSong = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+  } catch (error) {}
 };
