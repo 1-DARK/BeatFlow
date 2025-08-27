633e9547145f80bb303926d530608229dbe0a4f9
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-08-27 22:40:41 +0530

    initial commit

--- backend/src/controller/song.controller.js
+++ backend/src/controller/song.controller.js
@@ -29,13 +29,55 @@ export const getFeaturedSongs = async (req, res, next) => {
       },
     ]);
     res.json(songs);
-  } catch (error) {}
+  } catch (error) {
+    next(error);
+  }
 };
 export const getMadeForYouSongs = async (req, res, next) => {
   try {
-  } catch (error) {}
+    // fetch random 4 songs using mongodb's aggregation pipeline
+    const songs = await Song.aggregate([
+      {
+        $sample: {
+          size: 4,
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+          title: 1,
+          artist: 1,
+          imageUrl: 1,
+          AudioUrl: 1,
+        },
+      },
+    ]);
+    res.json(songs);
+  } catch (error) {
+    next(error);
+  }
 };
 export const getTrendingSongs = async (req, res, next) => {
   try {
-  } catch (error) {}
+    // fetch random 4 songs using mongodb's aggregation pipeline
+    const songs = await Song.aggregate([
+      {
+        $sample: {
+          size: 4,
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+          title: 1,
+          artist: 1,
+          imageUrl: 1,
+          AudioUrl: 1,
+        },
+      },
+    ]);
+    res.json(songs);
+  } catch (error) {
+    next(error);
+  }
 };
