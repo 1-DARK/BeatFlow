e8877efc538be73b7043dbb3bee42219b0047348
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-08-27 22:50:47 +0530

    initial commit

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,11 +1,8 @@
 import { Router } from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
+import { getAllUsers } from "../controller/user.controller.js";
 const router = Router();
 
-router.get("/like", protectRoute, (req, res) => {
-  // protect route is used for auth if the user is auth then it will run the /like route
-  req.auth.userId;
-  res.send("user route");
-});
-
+router.get("/", protectRoute, getAllUsers);
+// todo get messages
 export default router;
