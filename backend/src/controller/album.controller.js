e5abba8e5c2e8f07a64bb33672083b62ad36d716
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-08-27 22:04:27 +0530

    initial commit

--- backend/src/controller/album.controller.js
+++ backend/src/controller/album.controller.js
@@ -1,2 +1,16 @@
-export const getAllAlbums = async (req, res) => {};
-export const getAlbumById = async (req, res) => {};
+import { Album } from "../models/album.model.js";
+
+export const getAllAlbums = async (req, res, next) => {
+  try {
+    const albums = await Album.find();
+    res.status(200).json(albums);
+  } catch (error) {
+    next(error);
+  }
+};
+export const getAlbumById = async (req, res, next) => {
+  try {
+    const { albumId } = req.params;
+    const album = await Album.findById(albumId).populate("songs"); //References in Schema
+  } catch (error) {}
+};
