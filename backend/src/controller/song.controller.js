1dc5285a68811a8c7d935a97a381ca4c245dbd64
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-08-27 22:30:14 +0530

    initial commit

--- backend/src/controller/song.controller.js
+++ backend/src/controller/song.controller.js
@@ -2,6 +2,22 @@ import { Song } from "../models/song.model.js";
 
 export const getAllSongs = async (req, res, next) => {
   try {
+    // -1 = Descending => newest -> oldest
     const songs = await Song.find().sort({ createdAt: -1 });
+    res.json(songs);
+  } catch (error) {
+    next(error);
+  }
+};
+export const getFeaturedSongs = async (req, res, next) => {
+  try {
+  } catch (error) {}
+};
+export const getMadeForYouSongs = async (req, res, next) => {
+  try {
+  } catch (error) {}
+};
+export const getTrendingSongs = async (req, res, next) => {
+  try {
   } catch (error) {}
 };
