e7bc3e953ace440d0df7adf79eaff0316bea7227
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-08-29 00:18:19 +0530

    initial commit

--- frontend/src/providers/AuthProvider.tsx
+++ frontend/src/providers/AuthProvider.tsx
@@ -1,5 +1,6 @@
 import { axiosInstance } from "@/lib/axios.ts";
 import { useAuth } from "@clerk/clerk-react";
+import { Loader } from "lucide-react";
 import { useEffect, useState } from "react";
 const updateApiToken = (token: string | null) => {
   if (token) {
@@ -9,18 +10,31 @@ const updateApiToken = (token: string | null) => {
     delete axiosInstance.defaults.headers.common["Authorization"];
   }
 };
-const AuthProvider = () => {
-  const { getToken, userId } = useAuth();
+const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const { getToken } = useAuth();
   const [loading, setLoading] = useState(true);
   useEffect(() => {
     const initAuth = async () => {
       try {
         const token = await getToken();
         updateApiToken(token);
-      } catch (error) {}
+      } catch (error) {
+        updateApiToken(null);
+        console.log("Error in Auth Provider");
+      } finally {
+        setLoading(false);
+      }
     };
-  }, []);
-  return <div></div>;
+    initAuth();
+  }, [getToken]);
+
+  if (loading)
+    return (
+      <div className="h-screen w-full flex items-center justify-center">
+        <Loader className="size-8 text-emerald-500 animate-spin" />
+      </div>
+    );
+  return <>{children}</>;
 };
 
 export default AuthProvider;
