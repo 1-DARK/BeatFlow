8e3793134c29d6181bf3cba5b20d8f0d36139ad0
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-08-28 23:10:02 +0530

    initial commit

--- backend/src/routes/stat.route.js
+++ backend/src/routes/stat.route.js
@@ -1,12 +1,36 @@
 import { Router } from "express";
 import { Song } from "../models/song.model.js";
 import { User } from "../models/user.model.js";
+import { Album } from "../models/album.model.js";
 const router = Router();
 
 router.get("/", async (req, res, next) => {
   try {
-    const totalSongs = await Song.countDocuments();
-    const totalUsers = await User.countDocuments();
+    // const totalSongs = await Song.countDocuments();
+    // const totalUsers = await User.countDocuments();
+    // const totalAlbums = await Album.countDocuments();
+    const [totalSongs, totalUsers, totalAlbums, uniqueArtists] =
+      await Promise.all([
+        Song.countDocuments(),
+        Album.countDocuments(),
+        User.countDocuments(),
+        Song.aggregate([
+          {
+            $unionWith: {
+              coll: "albums",
+              pipeline: [],
+            },
+          },
+          {
+            $group: {
+              _id: "$artist",
+            },
+          },
+          {
+            $count: "count",
+          },
+        ]),
+      ]);
   } catch (error) {}
 });
 
