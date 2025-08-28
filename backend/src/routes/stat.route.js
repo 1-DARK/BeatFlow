d21c0ee81e4b7bd1c1f2b45299227d20aebe7f05
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-08-28 23:02:08 +0530

    initial commit

--- backend/src/routes/stat.route.js
+++ backend/src/routes/stat.route.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
+import { Song } from "../models/song.model.js";
+import { User } from "../models/user.model.js";
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("stat");
+router.get("/", async (req, res, next) => {
+  try {
+    const totalSongs = await Song.countDocuments();
+    const totalUsers = await User.countDocuments();
+  } catch (error) {}
 });
 
 export default router;
