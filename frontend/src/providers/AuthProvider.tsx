53c2ab61b0e741281322b5b1db92c44f066dd1ef
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-08-29 00:04:44 +0530

    initial commit

--- frontend/src/providers/AuthProvider.tsx
+++ frontend/src/providers/AuthProvider.tsx
@@ -1,6 +1,14 @@
+import { axiosInstance } from "@/lib/axios.ts";
 import { useAuth } from "@clerk/clerk-react";
 import { useEffect, useState } from "react";
-
+const updateApiToken = (token: string | null) => {
+  if (token) {
+    // if i have no token i am not authorize
+    axiosInstance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  } else {
+    delete axiosInstance.defaults.headers.common["Authorization"];
+  }
+};
 const AuthProvider = () => {
   const { getToken, userId } = useAuth();
   const [loading, setLoading] = useState(true);
