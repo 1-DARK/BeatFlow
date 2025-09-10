06653d445b3d31a58e55c223daa9d44a86dc86e0
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-09-10 22:42:52 +0530

    initial commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,7 @@ import { connectDB } from "./lib/db.js";
 import cors from "cors";
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(
--- frontend/src/components/SignInOAuthButtons.tsx
+++ frontend/src/components/SignInOAuthButtons.tsx
@@ -3,25 +3,28 @@ import { Button } from "./ui/button";
 
 const SignInOAuthButtons = () => {
   const { signIn, isLoaded } = useSignIn();
+
   if (!isLoaded) {
     return null;
   }
+
   const signInWithGoogle = () => {
     signIn.authenticateWithRedirect({
       strategy: "oauth_google",
-      redirectUrl: "/sso-callback", // google authenticates you give credentials
+      redirectUrl: "/sso-callback",
       redirectUrlComplete: "/auth-callback",
     });
   };
+
   return (
     <Button
       onClick={signInWithGoogle}
       variant={"secondary"}
-      className="w-full text-white border-zinc-200 h-11 cursor-pointer"
+      className="w-full text-white border-zinc-200 h-11"
     >
+      <img src="/google.png" alt="Google" className="size-5" />
       Continue with Google
     </Button>
   );
 };
-
 export default SignInOAuthButtons;
