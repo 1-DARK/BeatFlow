951a5f4217eb0a7888d8c1806560a6b8fec24314
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-09-13 23:03:32 +0530

    initial commit

--- frontend/src/layout/components/LeftSidebar.tsx
+++ frontend/src/layout/components/LeftSidebar.tsx
@@ -2,12 +2,19 @@ import PlaylistSkeleton from "@/components/skeletons/PlaylistSkeleton";
 import { buttonVariants } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { cn } from "@/lib/utils";
+import { useMusicStore } from "@/store/useMusicStore";
 import { SignedIn } from "@clerk/clerk-react";
 import { HomeIcon, Library, MessageCircle } from "lucide-react";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const LeftSidebar = () => {
-  const isLoading = true;
+  const { songs, albums, fetchAlbums, isLoading } = useMusicStore();
+
+  useEffect(() => {
+    fetchAlbums();
+  }, [fetchAlbums]);
+  console.log({ albums });
   return (
     <div className="h-full flex flex-col gap-2">
       {/* Menu */}
