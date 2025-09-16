0279263c210dc5e90cb72c1083de7899d5ac272b
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-09-16 23:10:39 +0530

    initial commit

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,6 +1,23 @@
 import Topbar from "@/components/Topbar";
+import { useMusicStore } from "@/store/useMusicStore";
+import { useEffect } from "react";
 
 const HomePage = () => {
+  const {
+    isLoading,
+    fetchFeaturedSongs,
+    fetchMadeForYouSongs,
+    fetchTrendingSongs,
+    madeForYouSongs,
+    featuredSongs,
+    trendingSongs,
+  } = useMusicStore();
+  useEffect(() => {
+    fetchFeaturedSongs();
+    fetchMadeForYouSongs();
+    fetchTrendingSongs();
+  }, [fetchFeaturedSongs, fetchMadeForYouSongs, fetchTrendingSongs]);
+  console.log(isLoading, madeForYouSongs, featuredSongs, trendingSongs);
   return (
     <div className="rounded-md overflow-hidden">
       <Topbar />
