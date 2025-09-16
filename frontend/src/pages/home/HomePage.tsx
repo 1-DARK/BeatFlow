11201b0a48bae74991a047fdf6f9030d7bf5cdfe
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-09-16 23:38:47 +0530

    initial commit

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -2,6 +2,7 @@ import Topbar from "@/components/Topbar";
 import { useMusicStore } from "@/store/useMusicStore";
 import { useEffect } from "react";
 import FeaturedSection from "./components/FeaturedSection";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 const HomePage = () => {
   const {
@@ -20,10 +21,18 @@ const HomePage = () => {
   }, [fetchFeaturedSongs, fetchMadeForYouSongs, fetchTrendingSongs]);
   console.log(isLoading, madeForYouSongs, featuredSongs, trendingSongs);
   return (
-    <div className="rounded-md overflow-hidden">
+    <main className="rounded-md overflow-hidden h-full bg-gradient-to-b from-zinc-800 to-zinc-900 ">
       <Topbar />
-      <FeaturedSection />
-    </div>
+      <ScrollArea className="h-[calc(100vh-180px)]">
+        <div className="p-4 sm:p-6">
+          <h1 className="text-2xl sm:text-3xl font-bold mb-6">
+            Good Afternoon
+          </h1>
+          <FeaturedSection />
+        </div>
+        <div className="space-y-8 "></div>
+      </ScrollArea>
+    </main>
   );
 };
 
