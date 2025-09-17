947df1a3f58ee31934beb0cd037511c159850689
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-09-17 23:40:35 +0530

    initial commit

--- frontend/src/layout/components/AudioPlayer.tsx
+++ frontend/src/layout/components/AudioPlayer.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { usePlayerStore } from "@/store/usePlayerStore";
+import { useEffect, useRef } from "react"; // Store mutable value
 
 const AudioPlayer = () => {
-  return <div></div>;
+  const audioRef = useRef<HTMLAudioElement>(null);
+  const prevSongRef = useRef<string | null>(null);
+  const { currentSong, isPlaying, playNext } = usePlayerStore();
+  useEffect(() => {
+    if (isPlaying) audioRef.current?.play();
+    else audioRef.current?.pause();
+  }, [isPlaying]);
+
+  useEffect(() => {
+    const audio = audioRef.current;
+    const handleEnded = () => {
+      playNext();
+    };
+    audio?.addEventListener("ended", handleEnded);
+    return () => {
+      audio?.removeEventListener("ended", handleEnded);
+    };
+  }, [playNext]);
+  return <audio ref={audioRef} />;
 };
 
 export default AudioPlayer;
