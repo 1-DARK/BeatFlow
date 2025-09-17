1acbdcf5f86239d92be67ec195cbac506bb1a004
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-09-17 22:29:51 +0530

    initial commit

--- frontend/src/layout/MainLayout.tsx
+++ frontend/src/layout/MainLayout.tsx
@@ -6,6 +6,7 @@ import {
 import { Outlet } from "react-router-dom";
 import LeftSidebar from "./components/LeftSidebar";
 import FriendsActivity from "./components/FriendsActivity";
+import AudioPlayer from "./components/AudioPlayer";
 
 const MainLayout = () => {
   const isMobile = false;
@@ -13,8 +14,9 @@ const MainLayout = () => {
     <div className="h-screen bg-black text-white flex flex-col">
       <ResizablePanelGroup
         direction="horizontal"
-        className="flex flex-1 h-full overflow-hidden"
+        className="flex flex-1 h-full overflow-hidden p-2"
       >
+        <AudioPlayer />
         {/*Left Sidebar */}
         <ResizablePanel
           defaultSize={20}
