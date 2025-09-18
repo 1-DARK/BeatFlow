f74a52eb7abfaacc917326f45442ad8d050d0f96
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-09-18 23:07:10 +0530

    initial commit

--- frontend/src/pages/home/components/FeaturedSection.tsx
+++ frontend/src/pages/home/components/FeaturedSection.tsx
@@ -4,19 +4,23 @@ import PlayButton from "./PlayButton ";
 
 const FeaturedSection = () => {
   const { isLoading, featuredSongs, error } = useMusicStore();
-  if (error) return <p className="text-red-500 mb-4 text-lg">{error}</p>;
+
   if (isLoading) return <FeaturedGridSkeleton />;
+
+  if (error) return <p className="text-red-500 mb-4 text-lg">{error}</p>;
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-8">
       {featuredSongs.map((song) => (
         <div
           key={song._id}
-          className="flex items-center bg-zinc-800/50 rounded-md overflow-hidden hover:bg-zinc-700/50 transition-colors group cursor-pointer relative"
+          className="flex items-center bg-zinc-800/50 rounded-md overflow-hidden
+         hover:bg-zinc-700/50 transition-colors group cursor-pointer relative"
         >
           <img
             src={song.imageUrl}
             alt={song.title}
-            className="w-16 sm:w-20 h-16 sm:h-20 object-cover flex shrink-0"
+            className="w-16 sm:w-20 h-16 sm:h-20 object-cover flex-shrink-0"
           />
           <div className="flex-1 p-4">
             <p className="font-medium truncate">{song.title}</p>
@@ -28,5 +32,4 @@ const FeaturedSection = () => {
     </div>
   );
 };
-
 export default FeaturedSection;
