68c91d549061cba25281295b2b0094d142ce7af5
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-09-19 23:17:03 +0530

    initial commit

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 import FeaturedSection from "./components/FeaturedSection";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import SectionGrid from "./components/SectionGrid";
-import { usePlayerStore } from "@/store/usePlayerStore";
+
 import { useMusicStore } from "@/store/useMusicStore";
 
 const HomePage = () => {
@@ -14,28 +14,15 @@ const HomePage = () => {
     fetchTrendingSongs,
     isLoading,
     madeForYouSongs,
-    featuredSongs,
+
     trendingSongs,
   } = useMusicStore();
-
-  const { initializeQueue } = usePlayerStore();
 
   useEffect(() => {
     fetchFeaturedSongs();
     fetchMadeForYouSongs();
     fetchTrendingSongs();
   }, [fetchFeaturedSongs, fetchMadeForYouSongs, fetchTrendingSongs]);
-
-  useEffect(() => {
-    if (
-      madeForYouSongs.length > 0 &&
-      featuredSongs.length > 0 &&
-      trendingSongs.length > 0
-    ) {
-      const allSongs = [...featuredSongs, ...madeForYouSongs, ...trendingSongs];
-      initializeQueue(allSongs);
-    }
-  }, [initializeQueue, madeForYouSongs, trendingSongs, featuredSongs]);
 
   return (
     <main className="rounded-md overflow-hidden h-full bg-gradient-to-b from-zinc-800 to-zinc-900">
--- frontend/src/store/useMusicStore.ts
+++ frontend/src/store/useMusicStore.ts
@@ -1,6 +1,5 @@
 import { axiosInstance } from "@/lib/axios";
 import type { Album, Song } from "@/types";
-
 import { create } from "zustand";
 
 interface MusicStore {
