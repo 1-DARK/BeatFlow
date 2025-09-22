848587e67d90ee176797b8cc0da1690e72b5792e
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-09-22 23:25:27 +0530

    init

--- frontend/src/layout/components/PlaybackControls.tsx
+++ frontend/src/layout/components/PlaybackControls.tsx
@@ -12,8 +12,32 @@ const PlaybackControls = () => {
     audioRef.current = document.querySelector("audio");
     const audio = audioRef.current;
     if (!audioRef) return;
-    const updateTime = () => setCurrentTime(audio.currentTime);
-  }, []);
+    const updateTime = () => {
+      if (audio) setCurrentTime(audio.currentTime);
+    };
+    const updateDuration = () => {
+      if (audio) setDuration(audio.duration);
+    };
+    audio?.addEventListener("timeupdate", updateTime);
+    audio?.addEventListener("loadeddata", updateDuration);
+    const handleEnded = () => {
+      usePlayerStore.setState({ isPlaying: false });
+    };
+    audio?.addEventListener("ended", handleEnded);
+
+    return () => {
+      audio?.removeEventListener("timeupdate", updateTime);
+      audio?.removeEventListener("loadedmetadata", updateDuration);
+      audio?.removeEventListener("ended", handleEnded);
+    };
+  }, [currentSong]);
+
+  const handleSeek = (value: number[]) => {
+    if (audioRef.current) {
+      audioRef.current.currentTime = value[0];
+    }
+  };
+
   return <div>hi</div>;
 };
 
