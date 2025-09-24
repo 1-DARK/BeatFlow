c567629069474eeed94f981957c71c9c9a6f04eb
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-09-24 20:05:01 +0530

    init

--- frontend/src/pages/admin/components/AlbumsTabContent.tsx
+++ frontend/src/pages/admin/components/AlbumsTabContent.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { Album } from "lucide-react";
 
 const AlbumsTabContent = () => {
-  return <div>Albums</div>;
+  return (
+    <Card>
+      <CardHeader>
+        <div className="flex items-center justify-between">
+          <div>
+            <CardTitle className="flex items-center gap-2">
+              <Album className="h-5 w-5 text-emerald-500 " />
+            </CardTitle>
+          </div>
+        </div>
+      </CardHeader>
+    </Card>
+  );
 };
-
 export default AlbumsTabContent;
--- frontend/src/pages/admin/components/SongsTabContent.tsx
+++ frontend/src/pages/admin/components/SongsTabContent.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Music, Plus } from "lucide-react";
+import SongsTable from "./SongsTable";
 
 const SongsTabContent = () => {
-  return <div>Songs</div>;
+  return (
+    <Card>
+      <CardHeader>
+        <div className="flex items-center justify-between">
+          <div>
+            <CardTitle className="flex items-center gap-2">
+              <Music className="size-5 text-emerald-500 " />
+              Songs Library
+            </CardTitle>
+            <CardDescription>Manage Your Music Tracks</CardDescription>
+          </div>
+          <Button>
+            <Plus className="text-black" />
+          </Button>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <SongsTable />
+      </CardContent>
+    </Card>
+  );
 };
 
 export default SongsTabContent;
