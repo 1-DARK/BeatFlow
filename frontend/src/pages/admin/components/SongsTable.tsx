5b12d769e8de731084d96d7c6423aee370303432
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-09-24 20:25:43 +0530

    init

--- frontend/src/pages/admin/components/SongsTable.tsx
+++ frontend/src/pages/admin/components/SongsTable.tsx
@@ -1,6 +1,21 @@
-import React from "react";
+import { useMusicStore } from "@/store/useMusicStore";
 
 const SongsTable = () => {
+  const { songs, isLoading, error } = useMusicStore();
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center py-8">
+        <div className="text-zinc-400">Loading Songs...</div>
+      </div>
+    );
+  }
+  if (error) {
+    return (
+      <div className="flex items-center justify-center py-8">
+        <div className="text-red-400">{error}</div>
+      </div>
+    );
+  }
   return <div>hi</div>;
 };
 
