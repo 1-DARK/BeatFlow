1ee2911dc6a3adfc0130a62fa0ecb4c7411d8bfe
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-09-24 19:00:26 +0530

    init

--- frontend/src/pages/admin/components/StatsCard.tsx
+++ frontend/src/pages/admin/components/StatsCard.tsx
@@ -1,4 +1,4 @@
-import { Card } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 
 type StatsCardProps = {
   icon: React.ElementType;
@@ -10,11 +10,32 @@ type StatsCardProps = {
 const StatsCard = ({
   bgColor,
   label,
-  icon,
+  icon: Icon,
   value,
   iconColor,
 }: StatsCardProps) => {
-  return <Card></Card>;
+  return (
+    <Card
+      className="bg-zinc-800/50
+border-zinc-700/50
+hover:bg-zinc-800/80 transition-colors"
+    >
+      <CardContent className="p-6">
+        <div
+          className="
+        flex items-center gap-4 "
+        >
+          <div className={`p-3 rounded-lg ${bgColor}`}>
+            <Icon className={`size-6 ${iconColor}`} />
+          </div>
+          <div>
+            <p className="text-sm text-zinc-400">{label}</p>
+            <p className="text-2xl font-bold">{value}</p>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default StatsCard;
