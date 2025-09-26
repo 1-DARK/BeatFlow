d38060219ec78bf04fe695f4952ba45e83111f59
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-09-26 22:56:10 +0530

    init

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -8,4 +8,14 @@ export const initializeSocket = (server) => {
       credentials: true,
     },
   });
+  const userSockets = new Map(); // {userId:socketId}
+  const userActivities = new Map(); // {userId:activity}
+  io.on("connection", (socket) => {
+    socket.on("user_connected", (userId) => {
+      userSockets.set(userId, socket.id);
+      userActivities.set(userId, "Idle");
+      // broadcasts to all connected sockets that this user just logged in
+      io.emit("user_connected", userId);
+    });
+  });
 };
