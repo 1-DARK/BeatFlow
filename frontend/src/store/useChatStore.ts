38cd96335c5551295ee699aa1ec88944ab5cc392
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-10-06 01:14:52 +0530

    deploy

--- frontend/src/store/useChatStore.ts
+++ frontend/src/store/useChatStore.ts
@@ -23,8 +23,8 @@ interface ChatStore {
   setSelectedUser: (user: User | null) => void;
 }
 
-const baseURL =
-  import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
+const baseURL = import.meta.env.VITE_API_URL;
+// same URL for production
 
 const socket = io(baseURL, {
   autoConnect: false, // only connect if user is authenticated
